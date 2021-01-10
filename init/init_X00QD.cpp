c2e9ebf917ccec45a97da11db2102b3715e34353
MLZ <76778799+MLZ94@users.noreply.github.com>
2021-01-10 21:08:17 +0100

    Update init_X00QD.cpp

--- init/init_X00QD.cpp
+++ init/init_X00QD.cpp
@@ -107,8 +107,8 @@ void vendor_check_variant()
     product_name = "ASUS_X00QD";
     product_device = "ASUS_X00QD";
     product_model = "ASUS_X00QD";
-    build_fingerprint = "google/coral/coral:11/RQ1A.201205.008/6943376:user/release-keys";
-    build_description = "coral-user 11 RQ1A.201205.008 6943376 release-keys";
+    build_fingerprint = "google/redfin/redfin:11/RQ1A.201015.003/7005429:user/release-keys";
+    build_description = "redfin-user 11 RQ1A.201015.003 7005429 release-keys";
 
     // Override props based on values set
     property_override("ro.build.description", build_description);
