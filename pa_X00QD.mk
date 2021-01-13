92dcfb018e1446b736333eed570fd0f8df82d125
MLZ <76778799+MLZ94@users.noreply.github.com>
2021-01-13 09:49:02 +0100

    Update pa_X00QD.mk

--- pa_X00QD.mk
+++ pa_X00QD.mk
@@ -24,7 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Inherit some AEX stuff.
 
-$(call inherit-product, vendor/pa/common.mk)
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
 
 
 # Inherit from X00QD device
